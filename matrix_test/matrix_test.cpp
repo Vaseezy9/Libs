c5c4fd81918d1a3c77d12886654e48ff0f4e63ec
Vaseezy9 <139249278+Vaseezy9@users.noreply.github.com>
2025-07-24 21:36:51 +0300

    Create matrix_test.cpp

---
+++ matrix_test/matrix_test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "matrix.h"
+
+int main() {
+    Matrix A(2, 2);
+    A(0, 0) = 1; A(0, 1) = 2;
+    A(1, 0) = 3; A(1, 1) = 4;
+
+    Matrix B(2, 2);
+    B(0, 0) = 5; B(0, 1) = 6;
+    B(1, 0) = 7; B(1, 1) = 8;
+
+    Matrix C = A * B;
+    std::cout << C << std::endl;
+
+    return 0;
+}
